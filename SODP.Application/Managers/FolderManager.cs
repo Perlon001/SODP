0d711c7d81e966defa9fa4192e1d3ab27a752ac9
Perlon001 <perlon@op.pl>
2021-04-11 22:55:29 +0200

    20210411

--- SODP.Application/Managers/FolderManager.cs
+++ SODP.Application/Managers/FolderManager.cs
@@ -52,41 +52,15 @@ namespace SODP.Application.Managers
             return (String.Format("Project {0} not exist", project.Symbol), false);
         }
 
-        //public async Task<(string Command, bool Success)> CreateOrUpdateFolderAsync(Project project)
-        //{
-        //    var (Command, Success) = GetCreateOrRenameFolderCommand(project);
-        //    if (Success)
-        //    {
-        //        return (Command, await FolderOperationTask(Command, project.ToString()));
-        //    }
-            
-        //    return (Command, false);
-        //}
-
         public async Task<(string Command, bool Success)> DeleteFolderAsync(Project project)
         {
             var command = _folderCommandCreator.GetDeleteFolderCommand(project);
             var catalog = GetFolders(project);
             if (catalog.Count() == 1)
             {
-                return (command, !await FolderOperationTask(command, project.ToString()));
+                return (command, await FolderOperationTask(command, project.ToString(), false));
             }
             return (command, false);
-
-            //var (Command, Success) = GetDeleteFolderCommand(project);
-            //if(Success)
-            //{
-            //    if(Command.Equals("Nothing to do."))
-            //    {
-            //        return (Command, true);
-            //    }
-            //    else
-            //    {
-            //        return (Command, !await FolderOperationTask(Command, project.ToString()));
-            //    }
-            //}
-
-            //return (Command, false);
         }
 
         public async Task<(string Command, bool Success)> ArchiveFolderAsync(Project project)
@@ -95,24 +69,9 @@ namespace SODP.Application.Managers
             var catalog = GetFolders(project);
             if (catalog.Count() == 1)
             {
-                return (command, !await FolderOperationTask(command, project.ToString()));
+                return (command, await FolderOperationTask(command, project.ToString(), false));
             }
             return (command, false);
-
-            //var (Command, Success) = GetArchiveFolderCommand(project);
-            //if (Success)
-            //{
-            //    if (Command.Equals("Nothing to do."))
-            //    {
-            //        return (Command, true);
-            //    }
-            //    else
-            //    {
-            //        return (Command, !await FolderOperationTask(Command, project.ToString()));
-            //    }
-            //}
-
-            //return (Command, false);
         }
 
         private Task<bool> FolderOperationTask(string command, string folder, bool exist = true)
@@ -123,80 +82,6 @@ namespace SODP.Application.Managers
                 return Directory.Exists(_projectFolder + folder) == exist;
             });
         }
-
-        private (string Command, bool Success) GetCreateOrRenameFolderCommand(Project project)
-        {
-            var unixCreateProjectArg = _projectFolder + project.Symbol + " _" + project.Title.Trim();
-            var unixRenameProjectArg = _projectFolder + project.ToString();
-            var catalog = GetFolders(project);
-            switch (catalog.Count())
-            {
-                case 0:
-                    {
-                        return (" " + _configuration.GetSection("AppSettings:CreateCommand").Value + " " +
-                            (Environment.OSVersion.Platform.Equals(PlatformID.Win32NT) ? _projectFolder + " " + project.Symbol + " _" + project.Title.Trim() : unixCreateProjectArg), true);
-                    }
-                case 1:
-                    {
-                        return (" " + _configuration.GetSection("AppSettings:RenameCommand").Value + " " +
-                            (Environment.OSVersion.Platform.Equals(PlatformID.Win32NT) ? _projectFolder + " " + Path.GetFileName(catalog[0]) + " " + project.ToString() : catalog[0] + " " + unixRenameProjectArg), true);
-                    }
-                default:
-                    {
-                        return ("Too many folders.", false);
-                    }
-            }
-        }
-
-        //private (string Command, bool Success) GetDeleteFolderCommand(Project project)
-        //{
-        //    (string, bool) result;
-        //    var projectPath = _projectFolder + project.ToString();
-        //    var catalog = GetFolders(project.Number, project.Stage.Sign);
-        //    switch(catalog.Count())
-        //    {
-        //        case 0:
-        //            result = ("Nothing to do.", true);
-        //            break;
-        //        case 1:
-        //            if (FolderIsEmpty(projectPath))
-        //            {
-        //                result = (" " + _configuration.GetSection("AppSettings:RemoveCommand").Value + " " +
-        //                    (Environment.OSVersion.Platform.Equals(PlatformID.Win32NT) ? _projectFolder + " " + project.ToString() : projectPath), true);
-        //            }
-        //            else
-        //            {
-        //                result = ("Folder is not empty.", false);
-        //            }
-        //            break;
-        //        default:
-        //            result = ("Too many folders.", false);
-        //            break;
-        //    }
-
-        //    return result;
-        //}
-        //private (string Command, bool Success) GetArchiveFolderCommand(Project project)
-        //{
-        //    (string, bool) result;
-        //    var projectPath = _projectFolder + project.ToString();
-        //    var catalog = GetFolders(project.Number, project.Stage.Sign);
-        //    switch (catalog.Count())
-        //    {
-        //        case 0:
-        //            result = ("Nothing to do.", true);
-        //            break;
-        //        case 1:
-        //            result = (" " + _configuration.GetSection("AppSettings:ArchiveCommand").Value + " " +
-        //                (Environment.OSVersion.Platform.Equals(PlatformID.Win32NT) ? _projectFolder + " " + project.ToString() : projectPath), true);
-        //            break;
-        //        default:
-        //            result = ("Too many folders.", false);
-        //            break;
-        //    }
-
-        //    return result;
-        //}
 
         private bool FolderIsEmpty(string folder)
         {

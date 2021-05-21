9e43f5bbee4cb803af3474efbdbb95ac12f5b38c
Jan Kowalski <perlon@op.pl>
2021-05-21 07:38:55 +0200

    20210521_ModifyJSActiveProject

--- SODP.UI/wwwroot/js/activeProjectList.js
+++ SODP.UI/wwwroot/js/activeProjectList.js
@@ -1,55 +1,5 @@
-﻿var myTable;
-$(document).ready(function () {
-    InitProjectDataTable();
-});
-
-function RenderButton(data) {
-    var renderTags = `<div class="row group justify-content-center">`;
-    renderTags += `<a onclick = 'Delete("/api/ActiveProjects/${data}")' class='btn btn-sm btn-danger text-white mb-0 mt-0 ml-1 mr-1 p-1' style = "cursor:pointer; width=70px;" data-toggle="tooltip" data-placement="top" title="Usuń projekt"> <i class="far fa-trash-alt"></i></a >`;
-    renderTags += `<a onclick = 'Archive("/api/ActiveProjects/${data}")' class='btn btn-sm btn-success text-white mb-0 mt-0 ml-1 mr-1 p-1' style = "cursor:pointer; width=70px;" data-toggle="tooltip" data-placement="top" title="Przenieś projekt do archiwum"> <i class="fas fa-archive"></i></a >`;
-    renderTags += `</div >`;
-
-    return renderTags
-}
-
-function InitProjectDataTable() {
-    myTable = $('#ProjectDataTable').DataTable({
-        //ajax: {
-        //    url: "/api/Projects",
-        //    type: "GET",
-        //    dataSrc: 'data.collection',
-        //},
-        data: ProjectsArray,
-        columns: [
-            { data: "number" },
-            { data: "stageSign" },
-            {
-                data: "title",
-                render: function (data, type, row) {
-                    //return `<a href="/ActiveProjects/CreateUpdate?id=${row.id}" title="Edycja nazwy">${row.title}</a>`;
-                    return `<a onclick="OpenProjectEdit(${row.id})" data-toggle="tooltip" title="Edycja nazwy">${row.title}</a>`;
-                }
-            },
-            {
-                data: "id",
-                render: function (data) {
-                    return RenderButton(data);
-                },
-                width: "15%",
-                visible: RoleCheck
-            }
-        ]
-    });
-    // this line is to watch the result in console , you can remove it later	
-    // window.location.replace("/Projects");
-}
-
-
-$(function () {
-    var placeholderElement = $('#modal-placeholder');
-
+﻿$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
-
     $('button[data-toggle="ajax-modal"]').click(function (event) {
         var url = $(this).data('url');
         $.get(url).done(function (data) {
@@ -58,7 +8,45 @@ $(function () {
         });
     });
 
+    InitProjectDataTable();
+    InitModalPlaceHolder();
+
+});
+
+function InitProjectDataTable() {
+    $('#ProjectDataTable').DataTable({
+        data: ProjectsArray,
+        columns: [
+            { data: "number" },
+            { data: "stageSign" },
+            {
+                data: "title",
+                render: function (data, type, row) {
+                    return `<a onclick="OpenProjectEdit(${row.id})" data-toggle="tooltip" title="Edycja nazwy">${row.title}</a>`;
+                }
+            },
+            {
+                data: "id",
+                render: function (data) {
+                    var renderTags = `<div class="row group justify-content-center">`;
+                    renderTags += `<a onclick = 'Delete("/api/ActiveProjects/${data}")' class='btn btn-sm btn-danger text-white mb-0 mt-0 ml-1 mr-1 p-1' style = "cursor:pointer; width=70px;" data-toggle="tooltip" data-placement="top" title="Usuń projekt"> <i class="far fa-trash-alt"></i></a >`;
+                    renderTags += `<a onclick = 'Archive("/api/ActiveProjects/${data}")' class='btn btn-sm btn-success text-white mb-0 mt-0 ml-1 mr-1 p-1' style = "cursor:pointer; width=70px;" data-toggle="tooltip" data-placement="top" title="Przenieś projekt do archiwum"> <i class="fas fa-archive"></i></a >`;
+                    renderTags += `</div >`;
+
+                    return renderTags
+                },
+                width: "15%",
+                visible: RoleCheck
+            }
+        ]
+    });
+}
+
+function InitModalPlaceHolder() {
+    var placeholderElement = $('#modal-placeholder');
+
     placeholderElement.on('click', '[data-save="modal"]', function (event) {
+        console.log(event);
         event.preventDefault();
         var form = $(this).parents('.modal').find('form');
         var actionUrl = form.attr('action');
@@ -72,4 +60,4 @@ $(function () {
             }
         });
     });
-})
+}
